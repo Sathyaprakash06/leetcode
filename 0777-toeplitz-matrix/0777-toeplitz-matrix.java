277f0640a1ec6eb20d87cc6169b118c98b2afbf7
Sathyaprakash06 <sathyaprakash0607@gmail.com>
2025-04-06 12:33:49 +0530

    Time: 0 ms (100%), Space: 44.6 MB (37.21%) - LeetHub

---
+++ 0777-toeplitz-matrix/0777-toeplitz-matrix.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        int i,j;
+        for( i=0;i<matrix.length-1;i++){
+            for( j=0;j<matrix[i].length-1;j++){
+                if(matrix[i][j]!=matrix[i+1][j+1]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
