321da4ba398ca5ea8e3246e1c34809ae34bc4910
Sathyaprakash S <sathyaprakash0607@gmail.com>
2025-07-14 14:35:06 +0530

    Time: 259 ms (26.14%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 select  c1.*
 from cinema c1
-where  c1 . id % 2= 1 and c1.description <> 'boring'
-order by c1.rating desc;
+where c1.id% 2= 1 and c1.description<>'boring'
+order by c1.rating desc
