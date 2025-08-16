34850b1cee010dab93933d96887c812adae7b05d
Sathyaprakash S <sathyaprakash0607@gmail.com>
2025-08-16 19:35:45 +0530

    Time: 0 ms (100%), Space: 42.2 MB (7.91%) - LeetHub

---
+++ 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.java
@@ -0,0 +1,11 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        HashMap<Character,Integer>map=new HashMap<>();
+        for(char i:s.toCharArray()){
+        map.put(i,map.getOrDefault(i,0)+1);
+        if(map.get(i)==2){
+            return i;
+        }}
+        return '0';
+    }
+}
