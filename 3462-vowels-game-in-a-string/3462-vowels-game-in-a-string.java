b57638e1183a22e44d3ed6cbf78dde8951fc3443
Sathyaprakash S <sathyaprakash0607@gmail.com>
2025-09-12 10:03:08 +0530

    Time: 6 ms (65.62%), Space: 45.5 MB (82.68%) - LeetHub

---
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean doesAliceWin(String s) {
+        
+        int count=0;
+        for(char i:s.toCharArray()){
+            if(i=='a'||i=='e'||i=='o'||i=='i'||i=='u')
+                count++;
+            }
+            if(count==0){
+            return false;
+            }
+            
+                return true;
+        
+    }
+}
