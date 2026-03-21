7c98e6d0adac6dec046206e9ff48b034cfdb8443
Sathyaprakash S <sathyaprakash0607@gmail.com>
2026-03-21 14:21:31 +0530

    Time: 2 ms (84.2%), Space: 45.1 MB (10.18%) - LeetHub

---
+++ 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int minimumPairRemoval(int[] nums) {
+        PriorityQueue<Integer>pq=new PriorityQueue();
+        int c=0;
+        int n=nums.length;
+        while(!bol(nums)){
+            int[]arr=new int[n-1];
+            int min=Integer.MAX_VALUE;
+            int ind1=0;
+            for(int i=0;i<nums.length-1;i++){
+                if(min>(nums[i]+nums[i+1])){
+                    min=nums[i]+nums[i+1];
+                    ind1=i;
+                }
+            }
+            int ind=0;
+            for(int i=0;i<nums.length;i++){
+                if(i==ind1){
+                    arr[ind++]=min;
+                    i++;
+                }else{
+                    arr[ind++]=nums[i];
+                }
+            }
+            nums=arr;
+            n--;
+           c++;
+        }
+        return c;
+    }
+    public static boolean bol(int[]arr){
+        int c=0;
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]>arr[i+1]){
+           return false;
+            }
+        }
+        return true;
+    }
+}
